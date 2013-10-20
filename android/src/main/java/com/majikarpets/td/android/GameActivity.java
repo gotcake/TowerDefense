e3d1647b8fa5821503a9134173d0dc1d392f5085
Aaron Cake <aaron.cake@gmail.com>
2013-10-20 16:09:25 -0400

    first commit

---
+++ android/src/main/java/com/majikarpets/td/android/GameActivity.java
@@ -0,0 +1,19 @@
+package com.majikarpets.td.android;
+
+import com.majikarpets.td.core.Game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
+public class GameActivity extends AndroidApplication {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+			config.useGL20 = true;
+			initialize(new Game(), config);
+	}
+}
---
+++ desktop/src/main/java/com/majikarpets/td/java/GameDesktop.java
@@ -0,0 +1,14 @@
+package com.majikarpets.td.java;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+import com.majikarpets.td.core.Game;
+
+public class GameDesktop {
+	public static void main (String[] args) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.useGL20 = true;
+		new LwjglApplication(new Game(), config);
+	}
+}
